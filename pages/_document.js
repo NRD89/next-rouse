6185a00be8ea7bbbfaf3efb78a080b44526c11e1
Nathan Dalton <ndaltondev@gmail.com>
2021-10-18 21:26:40 -0700

    update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -56,7 +56,7 @@ export default class MyDocument extends Document {
             <img
               height="1"
               width="1"
-              style="display:none"
+              style={{ display: "none" }}
               src="https://www.facebook.com/tr?id=564321418233630&ev=PageView&noscript=1"
             />
           </noscript>
